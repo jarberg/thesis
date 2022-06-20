f2cc35fafb3ef533a7366994a1af660c1fd09f64
mjarb <mjarberg88@gmail.com>
2022-06-20 15:09:36 +0200

    fixed needless platform requirements for opengl
    removed unused dublicate code in inits

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,16 @@
+import distutils
+
 from setuptools import setup
 from setuptools import find_packages
 from setuptools import find_namespace_packages
 from setuptools import setup, find_packages
-
+print(distutils.util.get_platform())
 setup(
     name='your_project_name',
     version='0.1',
     description='A description.',
     packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
-    package_data={'': ['box.png','charlie.txt', r"PyOpenGL-3.1.5-py38-none-win_amd64.whl"]},
+    package_data={'': ['box.png','charlie.txt', r"PyOpenGL-3.1.5-py38-none-win_amd86.whl"]},
     include_package_data=True,
     entry_points = {'console_scripts': ['Package = src.__main__:main'],},
     install_requires=[],
